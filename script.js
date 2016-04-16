44fbe24c2cd602b52c2b12bd49f702436e4d408c
Stephen Song <stephen.song@gatech.edu>
2016-04-16 14:08:58 -0400

    Implemented homepage html, project styling, and search

---
+++ script.js
@@ -0,0 +1,8 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+
+var topic = decodeURIComponent($.urlParam('topic'));
+
+console.log(topic);
