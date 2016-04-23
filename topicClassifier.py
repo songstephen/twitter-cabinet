80f3f31c67f6333645f7297fdfa9b9cb59cc5fcb
Hamilton Greene <hamilton.greene@gmail.com>
2016-04-23 11:06:40 -0400

    Added basic word-matching algorithm for topic classification

--- topicClassifier.py
+++ topicClassifier.py
@@ -9,72 +9,90 @@ import timeit
 # and given set of topics/examples of those topics
 
 #Topic Training
+mode = "match"
+debug = True
 
-topicSet = [
-	('Should Congress raise the debt ceiling', 'Debt Ceiling'),
-	('Should the U.S. withdraw troops from Afghanistan?', 'Afghanistan'),
-	('Should the U.S. intervene in the affairs of other countries?', 'Foreign Intervention'),
-	('Should the U.S. have bailed out the major banks during the financial crisis of 2008?',
-		'Financial Regulation'),
-	('Do you approve or disapprove with how Obama is handling the economy?',
-		'Obama Economic Policy'),
-	('Should we expand or dismatle our Medicare program?', 'Medicare'),
-	('Should the federal government regulate the internet to deter online piracy?',
-		'Internet'),
-	('Do you believe the theory of Evolution?', 'Evolution'),
-	('Should federal government limit funds to public schools that do not \
-		meet peformance standards?', 'Education Funding'),
-	('Should the federal government fund stem cell research?', 'Stem Cell Research'),
-	('Should employers be required to pay men and women the same salary for the same job?',
-		'Equal Pay'),
-	('Should there be more restrictions on the current process of purchasing a gun?',
-		'Gun Control'),
-	('Should physically and mentally capable adults on welfare be required to work?',
-		'Welfare'),
-	('What is your stance on abortion?', 'Abortion'),
-	('Do you support the Patient Protection and Affordable Care Act Obamacare?', 
-		'Obamacare'),
-	('Do you support the legalization of same sex marriage?', 'Gay Marriage'),
-	('Should the government continue to fund Planned Parenthood?', 'Planned Parenthood'),
-	('Should the U.S. conduct targeted airstrikes on Iran nuclear weapon facilities?',
-		'Iran Airstrikes'),
-	('Should illegal immigrants have access to government-subsidized healthcare?',
-		'Immigration Healthcare'),
-	('Should people on the "no-fly list" be banned from purchasing guns and ammunition?',
-		'No-Fly List Gun Control'),
-	('Should Muslim immigrants be banned from entering the country until the government \
-		improves its ability to screen', 'Muslim Immigrants'),
-	('Should the government increase environmental regulations to prevent global warming?',
-		'Global Warming'),
-	('Should the U.S. accept refugees from Syria?', 'Syrian Refugees'),
-	('Should a photo ID be required to vote?', 'Voter Fraud'),
-	('Should a business be able to deny service to a customer if the request conflicts \
-		with the owners religious beliefs?', 'Religious Freedom Act'),
-	('Should the government raise the federal minimum wage?', 'Minimum Wage'),
-	('Should foreign terrorism suspects be given constitutional rights?', 'Terrorism'),
-	('Should there be term limits set for members of Congress?', 'Term Limits'),
-	('Should the government decrease military spending?', 'Military Spending'),
-	('Should Apple unlock the iPhones of suspected terrorists for the FBI?', 'iPhone Encryption'),
-	('Should national parks be preserved and protected by the federal government?', 'National Parks'),
-	('Should illegal immigrants be offered in-state tuition rates at public colleges within their \
-		residing state?', 'In-State Tuition'),
-	('Should local police increase surveillance and patrol of Muslim neighborhoods?', 'Muslim Surveillance'),
-	('Should victims of gun violence be allowed to sui firearms dealers and manufacturers?',
-		'Gun Liability'),
-	('Do you support increasing taxes for the rich in order to reduce interest rates for \
-		student loans?', 'Student Loans'),
-	('Should the U.S. increase restrictions on its current border security policy?', 'Border Security'),
-	('Should the federal government require children to be vaccinated fro preventable \
-		diseases?', 'Mandatory Vaccinations'),
-	('Should the military be allowed to use enhanced interrogation techniques such as waterboarding',
-		'Torture'),
-	('Should immigrants be required to learn English?', 'Immigrant Assimilation'),
-	('Should businesses be required to provide paid leave for full-time employees during the birth of a child',
-		'Paid Sick Leave'),
-	('Should corporations and unions Super PACs be allowed to donate to political candidates?',
-		'Campaign Finance')
+if(mode == "naivebayes"):
+	topicSet = [
+		('Should Congress raise the debt ceiling', 'Debt Ceiling'),
+		('Should the U.S. withdraw troops from Afghanistan?', 'Afghanistan'),
+		('Should the U.S. intervene in the affairs of other countries?', 'Foreign Intervention'),
+		('Should the U.S. have bailed out the major banks during the financial crisis of 2008?',
+			'Financial Regulation'),
+		('Do you approve or disapprove with how Obama is handling the economy?',
+			'Obama Economic Policy'),
+		('Should we expand or dismatle our Medicare program?', 'Medicare'),
+		('Should the federal government regulate the internet to deter online piracy?',
+			'Internet'),
+		('Do you believe the theory of Evolution?', 'Evolution'),
+		('Should federal government limit funds to public schools that do not \
+			meet peformance standards?', 'Education Funding'),
+		('Should the federal government fund stem cell research?', 'Stem Cell Research'),
+		('Should employers be required to pay men and women the same salary for the same job?',
+			'Equal Pay'),
+		('Should there be more restrictions on the current process of purchasing a gun?',
+			'Gun Control'),
+		('Should physically and mentally capable adults on welfare be required to work?',
+			'Welfare'),
+		('What is your stance on abortion?', 'Abortion'),
+		('Do you support the Patient Protection and Affordable Care Act Obamacare?', 
+			'Obamacare'),
+		('Do you support the legalization of same sex marriage?', 'Gay Marriage'),
+		('Should the government continue to fund Planned Parenthood?', 'Planned Parenthood'),
+		('Should the U.S. conduct targeted airstrikes on Iran nuclear weapon facilities?',
+			'Iran Airstrikes'),
+		('Should illegal immigrants have access to government-subsidized healthcare?',
+			'Immigration Healthcare'),
+		('Should people on the "no-fly list" be banned from purchasing guns and ammunition?',
+			'No-Fly List Gun Control'),
+		('Should Muslim immigrants be banned from entering the country until the government \
+			improves its ability to screen', 'Muslim Immigrants'),
+		('Should the government increase environmental regulations to prevent global warming?',
+			'Global Warming'),
+		('Should the U.S. accept refugees from Syria?', 'Syrian Refugees'),
+		('Should a photo ID be required to vote?', 'Voter Fraud'),
+		('Should a business be able to deny service to a customer if the request conflicts \
+			with the owners religious beliefs?', 'Religious Freedom Act'),
+		('Should the government raise the federal minimum wage?', 'Minimum Wage'),
+		('Should foreign terrorism suspects be given constitutional rights?', 'Terrorism'),
+		('Should there be term limits set for members of Congress?', 'Term Limits'),
+		('Should the government decrease military spending?', 'Military Spending'),
+		('Should Apple unlock the iPhones of suspected terrorists for the FBI?', 'iPhone Encryption'),
+		('Should national parks be preserved and protected by the federal government?', 'National Parks'),
+		('Should illegal immigrants be offered in-state tuition rates at public colleges within their \
+			residing state?', 'In-State Tuition'),
+		('Should local police increase surveillance and patrol of Muslim neighborhoods?', 'Muslim Surveillance'),
+		('Should victims of gun violence be allowed to sui firearms dealers and manufacturers?',
+			'Gun Liability'),
+		('Do you support increasing taxes for the rich in order to reduce interest rates for \
+			student loans?', 'Student Loans'),
+		('Should the U.S. increase restrictions on its current border security policy?', 'Border Security'),
+		('Should the federal government require children to be vaccinated fro preventable \
+			diseases?', 'Mandatory Vaccinations'),
+		('Should the military be allowed to use enhanced interrogation techniques such as waterboarding',
+			'Torture'),
+		('Should immigrants be required to learn English?', 'Immigrant Assimilation'),
+		('Should businesses be required to provide paid leave for full-time employees during the birth of a child',
+			'Paid Sick Leave'),
+		('Should corporations and unions Super PACs be allowed to donate to political candidates?',
+			'Campaign Finance')
 
-]
+	]
+elif(mode == "match"):
+	topicSet = {
+		"Debt Ceiling": ['debt'],
+		"Fuel Efficiency": ['gas', 'natural', 'fuel', 'efficiency', 'alternative'],
+		"Equal Pay": ['equal', 'pay', 'woman'],
+		"Gun Control": ['gun', 'control'],
+		"Abortion": ['abortion', 'pro', 'life'],
+		"Welfare": ['welfare', 'social security'],
+		"Obamacare": ['obamacare'],
+		"Gay Marriage": ['gay', 'same sex', 'lgbtq', 'marriage'],
+		"Planned Parenthood": ['planned', 'parenthood', 'funding'],
+		"Global Warming": ['global', 'warming', 'climate', 'change']
+	}
+else:
+	sys.exit("EXITING: Unrecognized mode")
 
 class CSVFetcher():
 	def __init__(self, fileName):
@@ -99,6 +117,22 @@ class JSONFetcher():
 			for line in f:
 				yield json.loads(line)
 
+#Helper Functions
+def topicMatch(tweetText, topicSet):
+	tweetTopic = None
+	for topic in topicSet.keys():
+		print("HAMY: Topic... " + topic)
+		for word in topicSet[topic]:
+			print("HAMY: Word... " + word)
+			if word in tweetText.lower():
+				if(debug):
+					print("Matched Word: " + word)
+				tweetTopic = topic 
+				return tweetTopic
+	return tweetTopic
+
+
+#Main Loop
 if __name__ == "__main__":
 
 	timeStart = timeit.default_timer()
@@ -140,9 +174,11 @@ if __name__ == "__main__":
 	#Begin feed processing
 	feedsProcessed = 0
 	tweetsProcessed = 0
+	tweetsDiscarded = 0
 	topicsProcessed = {}
 
-	topicClassifier = NaiveBayesClassifier(topicSet)
+	if(mode == "naivebayes"):
+		topicClassifier = NaiveBayesClassifier(topicSet)
 
 	#HAMY: Probably broken on CSV inputs
 	for feed in feeds.fetch():
@@ -154,7 +190,18 @@ if __name__ == "__main__":
 				tweetID = tweet['id_str']
 				tweetText = tweet['text']
 
-				tweetTopic = topicClassifier.classify(tweetText.decode('utf-8'))
+				if(mode == "naivebayes"):
+					tweetTopic = topicClassifier.classify(tweetText.decode('utf-8'))
+				elif(mode == "match"):
+					tweetTopic = topicMatch(tweetText, topicSet)
+
+				if(tweetTopic is None):
+					if(debug):
+						print("TWEET DROPPED: Tweet didn't fall into specified topics")
+					tweetsDiscarded += 1
+					continue
+				elif(debug):
+					print("Topic: " + tweetTopic)
 
 				if tweetTopic in topicsProcessed:
 					topicsProcessed[tweetTopic] += 1
@@ -202,6 +249,7 @@ if __name__ == "__main__":
 
 	print("Feeds Classified: " + str(feedsProcessed))
 	print("Tweets Classified: " + str(tweetsProcessed))
+	print("Tweets Discarded: " + str(tweetsDiscarded))
 
 	print("Topic breakdown")
 	for topic in topicsProcessed:
